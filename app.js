cac57faa956d6e81b947f5c3e667869941683881
Vance McGrady <vancemcgrady@gmail.com>
2022-01-07 10:03:59 -0700

    loop through library array and display books functioning correctly.

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ function Book(title, author, numOfPages, haveRead) {
 }
 // adding method to book prototype
 Book.prototype.displayInfo = function () {
-  console.log(
-    `${this.title} by ${this.author}  is ${this.numOfPages} pages long and  ${
-      this.haveRead ? "I have indeed read it" : "I have yet to read it"
-    }`
-  );
+  return `${this.title} by ${this.author}  is ${
+    this.numOfPages
+  } pages long and  ${
+    this.haveRead ? "I have indeed read it" : "I have yet to read it"
+  }`;
 };
 
 //
@@ -22,8 +22,35 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-const book1 = new Book("Infinite Jest", "David F. Wallace", 3000, true);
-book1.displayInfo();
-addBookToLibrary(book1);
+function displayBooks(arr) {
+  const mainContainer = document.querySelector(".books-container");
 
-console.log(myLibrary);
+  arr.forEach((book) => {
+    const bookCard = document.createElement("div");
+    mainContainer.appendChild(bookCard);
+    bookCard.innerHTML = `<div class="book-card">
+    <h4>${book.title}</h4>
+
+    <h4>${book.author}</h4>
+
+    <h4>${book.numOfPages} pages</h4>
+
+    <h4>${
+      book.haveRead ? "Have Read This book" : "Have Not Read This Book"
+    }</h4>`;
+  });
+}
+
+const book1 = new Book("Infinite Jest", "David F. Wallace", 3000, true);
+
+const book2 = new Book(
+  "One Hundred Years of Solitude",
+  "Gabriel Garcia Marquez",
+  560,
+  true
+);
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+
+displayBooks(myLibrary);
