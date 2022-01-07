9ae7050cd74156fddabec5982843aa561ce6c13a
Vance McGrady <vancemcgrady@gmail.com>
2022-01-07 09:27:06 -0700

    basic page structure in place

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("testing");
+let myLibrary = [];
+
+//Book constructor
+
+function Book(title, author, numOfPages, haveRead) {
+  this.title = title;
+  this.author = author;
+  this.numOfPages = numOfPages;
+  this.haveRead = haveRead;
+}
+// adding method to book prototype
+Book.prototype.displayInfo = function () {
+  console.log(
+    `${this.title} by ${this.author}  is ${this.numOfPages} pages long and  ${
+      this.haveRead ? "I have indeed read it" : "I have yet to read it"
+    }`
+  );
+};
+
+//
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+const book1 = new Book("Infinite Jest", "David F. Wallace", 3000, true);
+book1.displayInfo();
+addBookToLibrary(book1);
+
+console.log(myLibrary);
