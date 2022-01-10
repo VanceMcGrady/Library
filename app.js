95e78d7345567ee8d83bef0b14b6ab68095b8cda
Vance McGrady <vancemcgrady@gmail.com>
2022-01-10 16:08:20 -0700

    displayBooks functionality working

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ let myLibrary = [
     numOfPages: 5000,
     haveRead: false,
   },
+  {
+    title: "Harry Potter and The Chamber of Secrets",
+    author: "J.K. Rowling",
+    numOfPages: 500,
+    haveRead: true,
+  },
 ];
 
 function Book(title, author, numOfPages, haveRead) {
@@ -29,7 +35,16 @@ function displayBooks(arr) {
 
   arr.forEach((book) => {
     const newCard = document.createElement("div");
+    newCard.innerHTML = `<div class="book-card">
+    <h3>${book.title}</h3>
+    <h3>${book.author}</h3>
+    <h3>${book.numOfPages}pgs</h3>
+    <h4>${
+      book.haveRead ? "I have read this book." : "I have NOT read this book."
+    }</h4>
+</div>`;
     libraryContainer.appendChild(newCard);
-    newCard.innerHTML = "<h4> TESTING </h4>";
   });
 }
+
+displayBooks(myLibrary);
